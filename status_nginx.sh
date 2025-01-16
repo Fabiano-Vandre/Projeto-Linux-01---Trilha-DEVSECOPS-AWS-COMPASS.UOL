03edd3fa136fc6c661c9e9b15bc74b827974d41d
Fabiano Vandre <fabiano@DESKTOP-K2MGB8K.>
2025-01-15 21:09:30 -0300

    criação do script

---
+++ status_nginx.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#
+servico="nginx"
+
+log_dir="/var/log/nginx"
+
+#
+data_hora=$(date '+%c')
+
+#
+log_online="$log_dir/servidor_online.log"
+
+#
+log_offline="$log_dir/servidor_offline.log"
+
+status=$(systemctl is-active nginx)
+
+if [ "$status" == "active" ]; then
+	estado="Online"
+	mensagem="O serviço $servico está Online."
+	log="Data: $data_hora | Serviço: $servico | Status: $estado | $mensagem"
+	echo "$log" > "$log_online"
+	echo "$servico Online"
+
+else
+	estado="Offline"
+	mensagem="O servico $servico está Offline."
+	log="Data: $data_hora | Serviço: $servico | Status: $estado | $mensagem"
+	echo "$log" > "$log_offline"
+	echo "$servico Offline"
+fi
+
